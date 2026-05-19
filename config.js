484d44cd1286b8031595fcaa068316a44135c26c
manpakaya <136682134+manpakaya@users.noreply.github.com>
2026-05-20 01:10:22 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "WYYlWLBa#ADdBqF6M38kF2tqtsHfg9PohDRjfq7a-nueDRIn2hAU",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/DANUWA-MD.png?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DANUWA-MD Is Alive Now😍*",
 BOT_OWNER: '94776121326',  // Replace with the owner's phone number
